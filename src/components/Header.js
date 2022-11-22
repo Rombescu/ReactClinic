64cee7e3aad5304d6971a09efb712be3a7ffea59
Robert Andrici <andricirobby@gmail.com>
2022-11-22 20:39:29 +0200

    Header halfway there

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import "../index.css";
+
+export default function Header() {
+  return (
+    <container className="HeaderContainer">
+      <container className="LeftContainer">
+        <div className="Icon">
+          <img
+            className="IconImage"
+            src="https://media.istockphoto.com/id/1004243378/vector/caduceus-health-symbol-asclepiuss-wand-icon-black-color-illustration-flat-style-simple-image.jpg?s=612x612&w=0&k=20&c=Mymt2MH3jtAIhYXUBfbm75l0bJfQdYTd6FBeaWa4vXU="
+          />
+          <a>Clinic of Wonders</a>
+        </div>
+        <div className="Appointment">
+          <img
+            className="AppointmentImage"
+            src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-clipboard-512.png"
+          />
+          <a>Make an appointment</a>
+        </div>
+      </container>
+      <container className="Navigation">
+        <a>Services</a>
+        <a>Departments</a>
+        <a>Contact us</a>
+      </container>
+    </container>
+  );
+}
---
+++ src/pages/ContactUs.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export default function ContactUs() {
+  return (
+    <div>
+      <p>Our Facebook page</p>
+      <p>Email us at:</p>
+      <p>Call our support line at:</p>
+    </div>
+  );
+}
